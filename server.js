0aa869ff0eedfec13684db31aa4f9c761da593fb
Mikeal Rogers <mikeal.rogers@gmail.com>
2014-02-20 12:25:14 -0800

    Adding leveldb.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var config =
   { '#node.js': ['node.js']
   , '#couchdb': ['couchdb']
   , '#pouchdb': ['pouchdb']
+  , '#leveldb': ['leveldb']
   }
 
 require('./')('irc.freenode.org', 'StackOverflowing', config, path.join(__dirname, 'cache.json'))
