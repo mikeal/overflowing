5b7b4d1875d6280c3f5dd6d13181207c1c43fadb
Mikeal Rogers <mikeal.rogers@gmail.com>
2014-02-20 12:02:30 -0800

    Adding pouch.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path')
 var config =
   { '#node.js': ['node.js']
   , '#couchdb': ['couchdb']
+  , '#pouchdb': ['pouchdb']
   }
 
 require('./')('irc.freenode.org', 'StackOverflowing', config, path.join(__dirname, 'cache.json'))
